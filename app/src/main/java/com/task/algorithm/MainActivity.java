9dd4fa0a6647a53842fc2e28af8df8036bed4126
Shafiq Patel <shafiqpatel@gmail.com>
2021-02-04 20:02:46 +0530

    Toast Added

--- app/src/main/java/com/task/algorithm/MainActivity.java
+++ app/src/main/java/com/task/algorithm/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Toast Added", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Toast Added is achanged", Toast.LENGTH_SHORT).show();
     }
 }
